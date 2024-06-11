cb84ed627b68be5e36705aa15ea6f3776d036c74
fifovalle <fifanaufal10@gmail.com>
2024-06-11 17:59:33 +0700

    modifikasi file modal see admin

--- src/admin/partials/modal-see-admin.php
+++ src/admin/partials/modal-see-admin.php
@@ -5,46 +5,58 @@
                 <h1 class="modal-title fs-5" id="lihatAdminLabel">Info Admin</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <?php
+            $adminModel = new Admin($koneksi);
+            $adminInfo = $adminModel->tampilkanDataAdmin();
+            ?>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-6 text-center ">
-                        <img src="../assets/img/arashmil.jpg" alt="Admin Photo" class="img-fluid rounded mb-3">
-                        <h3>Nama Admin</h3>
-                        <p class="text-muted">NIP Admin</p>
-                    </div>
-                    <div class="col-6">
-                        <h4>Info Selengkapnya</h4>
-                        <table class="table table-borderless">
-                            <tr>
-                                <th>Nama Lengkap:</th>
-                                <td>Nama Lengkap Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Tanggal Lahir:</th>
-                                <td>Tanggal Lahir Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Alamat:</th>
-                                <td>Alamat Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Jabatan:</th>
-                                <td>Jabatan Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Jenis Kelamin:</th>
-                                <td>Jenis Kelamin Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Nomor Telepon:</th>
-                                <td>Nomor Telepon Admin</td>
-                            </tr>
-                            <tr>
-                                <th>Umur:</th>
-                                <td>Umur Admin</td>
-                            </tr>
-                        </table>
-                    </div>
+                    <?php if (!empty($adminInfo)) : ?>
+                        <?php foreach ($adminInfo as $admin) : ?>
+                            <div class="col-6 text-center ">
+                                <img src="../uploads/<?php echo $admin['Foto_Admin']; ?>" alt="Admin Photo" class="img-fluid rounded mb-3">
+                                <h3>Nama Admin</h3>
+                                <p class="text-muted">NIP Admin</p>
+                            </div>
+                            <div class="col-6">
+                                <h4>Info Selengkapnya</h4>
+                                <table class="table table-borderless">
+                                    <tr>
+                                        <th>Nama Lengkap:</th>
+                                        <td><?php echo $admin['Nama_Lengkap_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Tanggal Lahir:</th>
+                                        <td><?php echo $admin['Tanggal_Lahir_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Alamat:</th>
+                                        <td><?php echo $admin['Alamat_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Jabatan:</th>
+                                        <td><?php echo $admin['Jabatan_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Jenis Kelamin:</th>
+                                        <td><?php echo $admin['Jenis_Kelamin_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Nomor Telepon:</th>
+                                        <td><?php echo $admin['Nomor_Telepon_Admin']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Umur:</th>
+                                        <td><?php echo $admin['Umur_Admin']; ?></td>
+                                    </tr>
+                                </table>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <tr>
+                            <td colspan="6" class="text-center text-danger fw-bold ">Tidak Ada Data Admin!</td>
+                        </tr>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
