08a715d7e78ec8ba816148f62989d34bf800ef9c
fifovalle <fifanaufal10@gmail.com>
2024-07-14 13:53:55 +0700

    Selesai

--- src/admin/partials/modal-edit-admin.php
+++ src/admin/partials/modal-edit-admin.php
@@ -26,13 +26,11 @@
                         <label for="suntingPeranAdmin" class="form-label">Peran Admin</label>
                         <select name="Peran_Admin" id="suntingPeranAdmin" class="form-select">
                             <?php if ($_SESSION['Peran_Admin'] === 'Super Admin') : ?>
-                                <option value="Super Admin" selected>Super Admin</option>
-                            <?php elseif ($_SESSION['Peran_Admin'] === 'Admin') : ?>
-                                <option value="Admin" selected>Admin</option>
-                            <?php else : ?>
                                 <option selected disabled>Pilih Peran</option>
                                 <option value="Super Admin">Super Admin</option>
                                 <option value="Admin">Admin</option>
+                            <?php else : ?>
+                                <option value="Admin" selected>Admin</option>
                             <?php endif; ?>
                         </select>
                     </div>
